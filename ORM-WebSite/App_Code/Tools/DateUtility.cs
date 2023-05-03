bd7845e3b6486d844fef54099fe15581b0f78211
nader <nhashemi6@gmail.com>
2023-05-03 13:39:44 +0330

    move date func to dateUtil

--- ORM-WebSite/App_Code/Tools/DateUtility.cs
+++ ORM-WebSite/App_Code/Tools/DateUtility.cs
@@ -5,12 +5,36 @@ using System.Linq;
 using System.Web;
 
 
-    public static class DateUtility
+public static class DateUtility
+{
+    public static string MiladiToShamsi(DateTime date)
     {
-        public static string MiladiToShamsi(DateTime date)
-        {
-            PersianCalendar pc = new PersianCalendar();
-            return string.Format("{0}_{1}_{2}", pc.GetYear(date),
-                pc.GetMonth(date), pc.GetDayOfMonth(date));
-        }
+        PersianCalendar pc = new PersianCalendar();
+        return string.Format("{0}_{1}_{2}", pc.GetYear(date),
+            pc.GetMonth(date), pc.GetDayOfMonth(date));
     }
+
+    public static string ToShamsi(this string filldate)
+    {
+        var farsiDate = string.Empty;
+        var pDate = new PersianCalendar();
+        if (filldate != string.Empty)
+        {
+            var datefilldate = Convert.ToDateTime(filldate);
+            string year = Convert.ToString(pDate.GetYear(datefilldate));
+            string month = String.Format(Convert.ToString(pDate.GetMonth(datefilldate)), "00");
+            string day = String.Format(Convert.ToString(pDate.GetDayOfMonth(datefilldate)), "00");
+            farsiDate = year + "/" + month + "/" + day;
+        }
+        return farsiDate;
+    }
+
+    public static DateTime ToMiladi(this string dateFA)
+    {
+        var stringArr = dateFA.Split('/');
+        var year = Convert.ToInt32(stringArr[0]);
+        var month = Convert.ToInt32(stringArr[1]);
+        var day = Convert.ToInt32(stringArr[2]);
+        return new DateTime(year, month, day, new PersianCalendar());
+    }
+}
